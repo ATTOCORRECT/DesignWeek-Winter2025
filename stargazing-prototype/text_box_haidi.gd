3591cb6091c8d476ac38179815cc3596532837ed
ChrisSalame <148483955+ChrisSalame@users.noreply.github.com>
2025-03-05 19:05:24 -0500

    attempted to make textbox
    textbox worked then it broke
    not too sure why. leaving it there

---
+++ stargazing-prototype/text_box_haidi.gd
@@ -0,0 +1,67 @@
+extends CanvasLayer
+
+@onready var textbox_container = $textBox_Haidi_Container
+@onready var start_symbol = $textBox_Haidi_Container/Panel/MarginContainer/HBoxContainer/startTextEdge
+@onready var end_symbol = $textBox_Haidi_Container/Panel/MarginContainer/HBoxContainer/endTextEdge
+@onready var label = $textBox_Haidi_Container/Panel/MarginContainer/HBoxContainer/text
+
+enum State {
+	READY,
+	READING,
+	FINISHED
+}
+
+var current_state = State.READY
+var text_queue = []
+
+func _ready():
+	print("Starting in READY State")
+	hide_textbox()
+	queue_text("first text test")
+	queue_text("second text test")
+
+func _process(delta):
+	match current_state:
+		State.READY:
+			if!text_queue.empty():
+				display_text()
+		State.READING:
+			if Input.is_action_just_pressed("ui_accept"):
+				label.percent_visible = 1.0
+				end_symbol.text = "-"
+				change_state(State.FINISHED)
+		State.FINISHED:
+			if Input.is_action_just_pressed("ui_accept"):
+				change_state(State.READY)
+				hide_textbox()
+
+func queue_text(next_text):
+	text_queue.push_back(next_text)
+
+func hide_textbox():
+	start_symbol.text = ""
+	end_symbol.text = ""
+	label.text = ""
+	textbox_container.hide()
+
+func show_textbox():
+	start_symbol.text = "-"
+	textbox_container.show()
+
+func display_text():
+	var next_text = text_queue.pop_front()
+	label.text = next_text
+	change_state(State.READING)
+	show_textbox()
+	change_state(State.FINISHED)
+
+func change_state(next_state):
+	current_state = next_state
+	match current_state:
+		State.READY:
+			print("changing to READY state")
+		State.READING:
+			print("changing to READING state")
+		State.FINISHED:
+			print("changing to FINISHED state")
+		
