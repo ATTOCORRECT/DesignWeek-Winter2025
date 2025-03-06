2d5df767689dc59ad08ee7ccfc11f0c330a8fae4
SleepTightEgger <134976603+SleepTightEgger@users.noreply.github.com>
2025-03-05 20:53:37 -0500

    Add files via upload
    Added dialogue manager

---
+++ stargazing-prototype/dialogue_manager/DialogueManager.gd
@@ -0,0 +1,21 @@
+extends Label
+@export var dialogueLines: Array[String]
+@onready var db = get_tree().get_first_node_in_group("db")
+@onready var index = 0
+var isActive : bool
+var timer := Timer.new()
+
+func _ready():
+	add_child(timer)
+	timer.autostart = false
+	timer.wait_time = 0.2
+	timer.timeout.connect(func(): on_timer_timeout())
+	timer.start()
+
+func on_timer_timeout():
+	if index < dialogueLines.size() - 1:
+		index += 1
+
+func _process(delta):
+	text = str(dialogueLines[index])
+pass
